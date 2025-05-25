5091af14d64cf17a3e99b9840b716a570fdb1a31
Tauqeer Rahim <tauqeer.rahim03@gmail.com>
2025-05-25 18:13:30 +0530

    Add StarBackground component and integrate it into Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { ThemeToggle } from "../components/ThemeToggle";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
 
 export const Home = () => {
     return (
@@ -6,7 +7,7 @@ export const Home = () => {
             {/*Theme Toggle*/}
             <ThemeToggle />
             {/*Background Effects*/}
-
+            <StarBackground />
             {/*Navbar*/}
 
             {/*Main Content*/}
