e6f58fc5464503f978ce4cb978190f2651433bea
Tauqeer Rahim <tauqeer.rahim03@gmail.com>
2025-05-25 19:59:20 +0530

    Add HeroSection component and integrate it into Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,24 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "@/components/HeroSection";
 
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/*Theme Toggle*/}
             <ThemeToggle />
+
             {/*Background Effects*/}
             <StarBackground />
+
             {/*Navbar*/}
             <Navbar />
-            {/*Main Content*/}
 
+            {/*Main Content*/}
+            <main>
+                <HeroSection />
+            </main>
             {/*Footer*/}
         </div>
     );
