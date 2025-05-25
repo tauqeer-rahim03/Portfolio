242ae1377cf5a6242c1eec9b346117b32aed0163
Tauqeer Rahim <tauqeer.rahim03@gmail.com>
2025-05-25 13:48:23 +0530

    Refactor Home component layout and add base styles for theming and utilities

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export const Home = () => {
     return (
-        <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900">
+        <div className="min-h-screen">
             {/*Theme Toggle*/}
 
             {/*Background Effects*/}
