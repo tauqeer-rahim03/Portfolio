5ec87467564add90a2c181592364e561c5fba2ff
Tauqeer Rahim <tauqeer.rahim03@gmail.com>
2025-05-26 15:46:49 +0530

    Add Footer component and integrate it into Home page layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { ArrowUp } from "lucide-react";
+
+export const Footer = () => {
+    return (
+        <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
+            <p className="text-sm text-muted-foreground">
+                &copy; {new Date().getFullYear()} Tauqeer Rahim, All Rights
+                Reserved.
+            </p>
+            <a
+                href="#hero"
+                className="p2 rounded-full bg-primary/10 hover:bg-primary/20 transition-colors text-primary"
+            >
+                <ArrowUp size={20} />
+            </a>
+        </footer>
+    );
+};
